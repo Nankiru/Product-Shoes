e98f12159b3365ec9db444b09786c0444e81774c
Nankiru <khemsopheanan09@gmail.com>
2025-03-02 16:50:24 +0700

    changed rest api

--- heroes.js
+++ heroes.js
@@ -1,4 +1,4 @@
-export const heroes = [
+ const heroes = [
     {
       id: 1,
       name: "Alucard",
--- script/index.js
+++
@@ -1,6 +0,0 @@
-import { Render } from "../components/Render.js";
-import { heroes } from "../data/heroes.js";
-
-let render = document.querySelector("#render");
-
-heroes.map((hero)=>(render.innerHTML+= Render(hero)));
